31f48ad1a84b988c6f1c9a48b79bb7cc5cf20cd7
Jhosep <python.00.hb@gmail,com>
2023-11-02 17:54:15 -0500

    avance del proyecto tesis 2 / 11

---
+++ js/interaccion.js
@@ -0,0 +1,17 @@
+document.getElementById('formulario').addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const usuario = document.getElementById('usuario');
+
+    if(usuario.value === 'renato') {
+        window.location.href = '/view/docente/asistencia.html';
+
+    }
+    else if(usuario.value === 'admin') {
+        window.location.href = '/view/administrador/editar.html';
+    }
+    else {
+        alert('no es la contra')
+    }
+
+})
